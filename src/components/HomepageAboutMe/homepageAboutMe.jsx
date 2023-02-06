8905868f3bad4451eca741a9e8367ac8c5f2fafe
Tanmay Dureja <tanmaydureja@gmail.com>
2023-02-07 03:00:03 +0530

    Add button to about me section

--- src/components/HomepageAboutMe/homepageAboutMe.jsx
+++ src/components/HomepageAboutMe/homepageAboutMe.jsx
@@ -4,6 +4,7 @@ import healingImage from "../../assets/images/healing-image.jpg";
 import backgroundImage from "../../assets/images/background.jpg";
 import leftquote from "../../assets/svg/lquote.svg";
 import rightquote from "../../assets/svg/rquote.svg";
+import { Link } from "react-router-dom";
 
 const features = [
   {
@@ -98,13 +99,20 @@ const HomepageAboutMe = () => {
               <br />- Philip Pullman
             </p>
           </div>
-          <div className="flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center">
             <p className="p-12 md:p-24 text-xl md:text-2xl text-emerald-700 font-semibold leading-8">
               My work as a healer is to shine the light on your journey and
               empower you to claim back your power. The unfolding miracles of
               change, hope and transformation that I see in my clients are
               treasures that I receive along the way.
             </p>
+            <Link to="/about">
+                <button 
+                    className="focus:outline-none focus:shadow-outline bg-emerald-600 text-white font-bold py-2 px-16 rounded-full cursor-pointer hover:bg-emerald-700"
+                >
+                    Learn More
+                </button>
+            </Link>
           </div>
         </div>
       </div>
