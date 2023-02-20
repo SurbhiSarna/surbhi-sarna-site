144645d60891452d4a0be56c23614bd270edecf6
Tanmay Dureja <tanmaydureja@gmail.com>
2023-02-20 22:27:33 +0530

    :bug: Fix HomePage Props Issue

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,7 @@ export default function Home(){
         <div>
             <Masthead />
             <HomepageAboutMe />
-            <SplitImageText props={props} />
+            <SplitImageText />
             <TestimonialsSlider />
             <HomepageMoreInfo />
         </div>
