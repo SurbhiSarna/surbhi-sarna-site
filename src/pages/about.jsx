6311ff85db69090a5a182e045e59667ffe5b01fb
Tanmay Dureja <tanmaydureja@gmail.com>
2023-02-26 01:14:49 +0530

    Add About Page Layout and Content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutPageMasthead from '../components/AboutPageMasthead/aboutPageMasthead';
+import AboutPageContent from '../components/AboutPageContent/aboutPageContent';
 
 export default function About(){
     return (
         <div>
             <h1>About</h1>
+            <AboutPageContent />
             <AboutPageMasthead />
         </div>
     )
