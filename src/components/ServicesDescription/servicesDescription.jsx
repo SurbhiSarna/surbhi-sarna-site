bdbd4fd086bc16da2de49445bbdda76cadbf330b
Tanmay Dureja <tanmaydureja@gmail.com>
2023-03-14 00:49:36 +0530

    :zap: Add Links to services page

--- src/components/ServicesDescription/servicesDescription.jsx
+++ src/components/ServicesDescription/servicesDescription.jsx
@@ -34,31 +34,21 @@ const ServicesDescription = () => {
   return (
     <div className="mx-auto">
       <div
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="py-16 mx-auto"
+        className="py-16 mx-auto bg-violet-700"
       >
-        <h2 className="md:text-4xl text-2xl font-bold text-slate-700 mb-8 mx-auto max-w-7xl">
+        <h2 className="md:text-4xl text-2xl font-bold text-white mb-8 mx-auto max-w-7xl">
           Here's how I can help you
         </h2>
 
-        <div className="container m-auto px-6 text-gray-500 md:px-12 xl:px-0 max-w-7xl">
-          <div className="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-5">
+        <div className="container m-auto px-6 text-gray-500 md:px-12 xl:px-0 max-w-2xl">
+          <div className="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-1">
             {features.map((feature) => (
               <Link to={feature.link}>
                 <div
                   key={feature.name}
-                  className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 h-44"
+                  className="bg-emerald-600 rounded-2xl shadow-xl px-4 py-8 sm:px-10 lg:px-8 hover:bg-white hover:text-emerald-600 text-xl md:text-2xl font-semibold text-white"
                 >
-                  <div className="space-y-4">
-                    <h3 className="text-2xl font-semibold text-slate-800">
-                      {feature.heading}
-                    </h3>
-                  </div>
+                  {feature.heading}
                 </div>
               </Link>
             ))}
